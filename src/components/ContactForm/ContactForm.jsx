3341524cd5b821812ff5663d87a242815c8f50c1
Anastasia Zhaboiedova <tsyfrak@gmail.com>
2022-05-16 18:17:53 +0300

    add handleChange metod

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,18 +5,25 @@ class Form extends Component {
     state = {
         name: "",
     }
+
+    handleChange = e => {
+        const { name, value } = e.currentTarget;
+        this.setState({ [name]: value });
+    }
     
-    
+     
     render() {
         return(
             <form>
                 <label htmlFor="">Name
                 <input
                     type="text"
-                    name="name"
+                        name="name"
+                        value={this.state.name}
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
+                        required
+                        onChange={this.handleChange}
                     />
                 </label>
                 <button type="submit">Add contact</button>
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,7 @@
+const ContactList = () => {
+    return (
+        <ul>
+            {}
+        </ul>
+    )
+}
